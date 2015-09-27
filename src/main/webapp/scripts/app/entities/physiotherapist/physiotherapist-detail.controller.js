92fd0a94081c4af3cb0cc274b58207b9d47a17b5
Julien Houeix <julienhoueix@gmail.com>
2015-09-27 19:11:48 +0200

    Show physiotherapist on map

--- src/main/webapp/scripts/app/entities/physiotherapist/physiotherapist-detail.controller.js
+++ src/main/webapp/scripts/app/entities/physiotherapist/physiotherapist-detail.controller.js
@@ -1,14 +1,20 @@
 'use strict';
 
 angular.module('kineappApp')
+
     .controller('PhysiotherapistDetailController', function ($scope, $rootScope, $stateParams, entity, Physiotherapist, Specialty) {
+    	
         $scope.physiotherapist = entity;
+        $scope.map = { center: { latitude: $scope.physiotherapist.latitude, longitude: $scope.physiotherapist.longitude }, zoom: 15 };
+        
         $scope.load = function (id) {
             Physiotherapist.get({id: id}, function(result) {
                 $scope.physiotherapist = result;
             });
         };
+        
         $rootScope.$on('kineappApp:physiotherapistUpdate', function(event, result) {
             $scope.physiotherapist = result;
         });
+
     });
